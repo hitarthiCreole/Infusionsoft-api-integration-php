ecf9b566f49cd34bb4d57ef0b7787f5743acd046
hitarthiCreole <hitarthi@creolestudios.com>
2017-11-11 14:35:08 +0530

    Create conn.cfg.php

---
+++ conn.cfg.php
@@ -0,0 +1,6 @@
+<?php
+  $connInfo = array('connectionName:applicationName:i:APIKEYGOESHERE:This is 
+  the connection for applicationName.infusionsoft.com');
+//connectionName is your app name
+//APIKEYGOESHERE is your infusiondoft api key
+?>
